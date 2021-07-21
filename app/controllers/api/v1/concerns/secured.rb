03c15965e8922db0f56c7b3f12ad6ef5c8a91f51
Rhelli <rory.hellier@googlemail.com>
2021-07-21 18:11:00 +0100

    Updates Auth0 Config, Creates New Namespaced Controllers and Models:
     - Creates new API::V1 namespaced user and properties controllers
     - Creates user and properties models
     - Creates new /app/lib/json_web_tokens.rb file for decoding jwt token recieved by frontend
     - Creares new controllers/api/v1/concerns methods for defining the authenticate_request! filter, scopes and auth_token methods

---
+++ app/controllers/api/v1/concerns/secured.rb
@@ -0,0 +1,37 @@
+module Secured
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :authenticate_request!
+  end
+
+  SCOPES = {
+    '/private' => nil,
+    '/private-scoped' => ['read:users']
+  }
+
+  private
+
+  def authenticate_request!
+    @auth_payload, @auth_header = auth_token
+
+    render json: { errors: ['Insufficient scope #securedconcern'] }, status: :unauthorized unless scope_included
+  rescue JWT::VerificiationError, JWT::DecodeError
+    render json: { errors: ['Not Authenticated #securedconcern'] }, status: :unauthorized
+  end
+
+  def scope_included
+    if SCOPES[request.env['PATH_INFO']] === nil
+      true
+    else
+      (String(@auth_payload['scope']).split(' ') & (SCOPES[request.env['PATH_INFO']])).any?.
+  end
+
+  def http_token
+    request.headers['Authorization'].split(' ').last unless !request.headers['Authorization'].present?
+  end
+
+  def auth_token
+    JsonWebToken.verify(http_token)
+  end
+end
